2bde1db590094e6064262fca2221fc0303c5359b
전종현 <electricboy94@naver.com>
2017-07-26 22:44:30 -0700

    170726
    ㅇ

--- Peter/testofbluetooth.py
+++ Peter/testofbluetooth.py
@@ -9,11 +9,7 @@ if __name__ == '__main__':
 
     while True:
         sensor_output = sensor_server.get_sensor_output()
-        try:
-            sensor_output.send(msg)
-        except Exception as e:
-            BTError.print_error(handler=client_handler, error=BTError.ERR_WRITE, error_message=repr(e))
-            client_handler.handle_close()
+        sensor_output.send
 
             # Sleep for 3 seconds
         sleep(3)
