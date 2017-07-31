4512d90a5f72ea811599efb7f890728caa0904da
전종현 <electricboy94@naver.com>
2017-07-30 23:36:27 -0700

    170730
    code of temperature

--- Peter/Temperature1sec.py
+++ Peter/Temperature1sec.py
@@ -1,6 +1,10 @@
 from time import sleep
 while True :
     raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-    print raw
+    v = 5 * 0.000244140625 * raw
+
+    t = (1000 * v) - 642
+
     sleep(1)
-    
+
+    print t
