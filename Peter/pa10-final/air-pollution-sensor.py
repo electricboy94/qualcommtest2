f9b709ca9514d0ecba27c0c0e3d47446da6cbb3e
전종현 <electricboy94@naver.com>
2017-08-08 22:48:37 -0700

    170808
    hi

--- Peter/pa10-final/air-pollution-sensor.py
+++ Peter/pa10-final/air-pollution-sensor.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
         r_msg = ""
         if args.output_format == "csv":
             # Create CSV message "'real-time', time, temp, SN1, SN2, SN3, SN4, PM25".
-            r_msg = "{},{},{},{},{},{},{}".format(epoch_time, temp, SN1, SN2, SN3, SN4, PM25)
+            r_msg = "{},{},{},{},{},{},{},{}".format(' 4e:71:9e:8c:89:00',epoch_time, temp, SN1, SN2, SN3, SN4, PM25)
         elif args.output_format == "json":
             # Create JSON message.
             output = {'MAC': ' 4e:71:9e:8c:89:00',
