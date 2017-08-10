5c8442dde7ecf1d9b09b7e2b3203950414d2e64b
전종현 <electricboy94@naver.com>
2017-08-09 22:51:48 -0700

    170809
    식 최종수정 과정

--- Peter/pa10-final/sensor/Sensor.py
+++ Peter/pa10-final/sensor/Sensor.py
@@ -149,21 +149,21 @@ class SensorServer(Thread):
             t0 = 550
             c0, c1 = self.read_sensor(0)
             # Channel 1 is not connected so we don't care about its output
-            temp = 1.22*c0 - t0
+            temp = c0 - t0
             logger.info("{} sensor outputs {} degree".format(self.sensor_names[0], temp))
             # Save output to the dict
             self.sensor_output[self.sensor_names[0]] = temp
 
             logger.info("Reading {} sensor...".format(self.sensor_names[1]))
             c2, c3 = self.read_sensor(1)
-            sn1 = ((1.22*c2-347)-(0.03*(1.22*c3-296)))*0.00375
+            sn1 = ((c2-347)-(0.03*(c3-296)))*0.00375
             logger.info("{} sensor outputs {} ppb".format(self.sensor_names[1], sn1))
             # Save output to the dict
             self.sensor_output[self.sensor_names[1]] = sn1
 
             logger.info("Reading {} sensor...".format(self.sensor_names[2]))
             c4, c5 = self.read_sensor(2)
-            sn2 = ((1.22*c4-295)-(1.18*(1.22*c5-282)))*4.386
+            sn2 = ((c4-295)-(1.18*(c5-282)))*4.386
             logger.info("{} sensor outputs {} ppb".format(self.sensor_names[2], sn2))
             # Save output to the dict
 
@@ -171,21 +171,21 @@ class SensorServer(Thread):
 
             logger.info("Reading {} sensor...".format(self.sensor_names[3]))
             c6, c7 = self.read_sensor(3)
-            sn3 = ((1.22*c6-345)-(1.15*(1.22*c7-255)))*3.145
+            sn3 = ((c6-345)-(1.15*(c7-255)))*3.145
             logger.info("{} sensor outputs {} ppb".format(self.sensor_names[3], sn3))
             # Save output to the dict
             self.sensor_output[self.sensor_names[3]] = sn3
 
             logger.info("Reading {} sensor...".format(self.sensor_names[4]))
             c8, c9 = self.read_sensor(4)
-            sn4 =((1.22*c8-391)-(0.18*(1.22*c9-390)))*2.506
+            sn4 =((c8-391)-(0.18*(c9-390)))*2.506
             logger.info("{} sensor outputs {} ppb".format(self.sensor_names[4], sn4))
             # Save output to the dict
             self.sensor_output[self.sensor_names[4]] = sn4
 
             logger.info("Reading {} sensor...".format(self.sensor_names[5]))
             c10, c11 = self.read_sensor(5)
-            c10 = c10 * (5.0 / 4096.0)
+            c10 = c10 * 1000
             pm25 = 0.518 + 0.00274*(240.0*(c10)**6 - 2491.3*(c10)**5 + 9448.7*(c10)**4 - 14840.0*(c10)**3 + 10684.0*(c10)**2 + 2211.8*(c10) + 7.9623)
             logger.info("{} sensor outputs {} ppb".format(self.sensor_names[5], pm25))
             # Save output to the dict
